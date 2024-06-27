98fe5ea98cd9f911391bee3c7d6a83cc0f950655
chierz <bonanwaeze@gmail.com>
2024-06-27 13:09:40 -0700

    redone

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,19 +7,14 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
-<<<<<<< HEAD
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
-=======
-GRANT ALL PRIVILIGES
-    ON `hbnb_test_db`.*
-    TO 'hbnb_test'@'localhost';
-GRANT SELECT
-    ON `performance_schema`.*
-    TO 'hbnb_test'@'localhost';
->>>>>>> ad3a2899f0523403239ab44d8caf6a5ab65e3941
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+
 FLUSH PRIVILEGES;
